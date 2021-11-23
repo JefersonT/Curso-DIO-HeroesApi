babbbd94c9bab5f34329d9fe3b4da2b20f43a972
JefersonT <jefersontorres.f@gmail.com>
2021-11-22 22:11:37 -0300

    First commit - Parte 1 a Parte 5

---
+++ src/main/java/com/dio/herosapi/constans/HeroesConstant.java
@@ -0,0 +1,8 @@
+package com.dio.herosapi.constans;
+
+public class HeroesConstant {
+    public static final String HEROES_ENDPOINT_LOCAL="/heroes";
+    public static final String ENDPOINT_DYNAMO="https://localhost:8080";
+    public static final String REGION_DYNAMO="us-gost-1";
+
+}
